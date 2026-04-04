72a69d937c93499aad20ebe218f2f8d988a7b9b4
Alex Halder <alexhalder2007@gmail.com>
2026-04-04 17:41:50 +0600

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed to '/' for Vercel. If going back to GitHub Pages, use '/portfolio-react/'
+  base: '/portfolio-react/',
 });
